08187643e4faac8998a50e8d679542785de31169
Carlos Rafael Lopes da Silva <carlosrafael.silva@correios.com.br>
2026-03-16 11:33:25 -0400

    SAGY 1.0

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,8 @@
+//development local
 const API_URL = import.meta.env.VITE_API_URL;
+
+//deploy
+//const API_URL = 'https://sagy.onrender.com';
 
 export async function getHome() {
     const response = await fetch(`${API_URL}/home/home/`);
